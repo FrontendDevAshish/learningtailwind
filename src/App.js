7c3b71ef29e6feb1c734da9984469eda22e0b74c
Ashish <kumar.29ashish.95.@gmail.com>
2025-12-29 23:03:58 +0530

    sasta darkmode

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+  const [count, setCount] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-       <h1>Topa le leo</h1>
+    <div className={count ? "App":"Appa"} >
+      <header className={count ? "App-header":"Appa-header"}>
+      {count?<h1>Topa le leo</h1>:<h1>Nahi loge!</h1>} 
+       <button onClick={() => setCount(lanura => !lanura)}>
+        Lick me 
+       </button>
       </header>
     </div>
   );
